c1162f3be67ae665fcff5bcfbcf013801b87eb16
Tetsu-16700 <carolina.moscosot@gmail.com>
2024-02-13 23:50:10 -0500

    Autenticacion de login

--- src/components/pages/login.pages.tsx
+++ src/components/pages/login.pages.tsx
@@ -1,8 +1,43 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { Button } from "../ui/button";
+// mensaje de alerta
+import { useToast } from "@/components/ui/use-toast";
 
 export default function LoginPage() {
+  // funcion para utlizar mensaje de alerta
+  const { toast } = useToast();
 
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSumit(e: FormEvent) {
+    e.preventDefault();
+    // valida que el input tenga valor
+    if (data.email.length && data.password.length) {
+      const response = fetchLogin();
+      if (!response) {
+        toast({
+          variant: "destructive",
+          description: "👀Uh oh! Something went wrong.",
+        });
+      } else {
+        toast({
+          description: "✅Your message has been successfully sent!",
+        });
+      }
+    }
+  }
+
+  //   funcion que captura evento
+  function fetchLogin() {
+    if (data.email === "abc@gmail.com" && data.password === "abc") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   //   estructura y visualizacion de login que podra observar el usuario
   return (
@@ -13,15 +48,14 @@ export default function LoginPage() {
           <form
             action=""
             className="flex flex-col gap-4"
-            
+            onSubmit={handleSumit}
           >
             <div className="flex flex-col gap-2">
               <label htmlFor="">Email</label>
               <input
                 className="px-2 py-1 border  rounded-md border-slate-500 outline-none"
                 type="text"
-                name=""
-                id=""
+                onChange={(e) => setData({ ...data, email: e.target.value })}
               ></input>
             </div>
             <div className="flex flex-col gap-2">
@@ -29,8 +63,7 @@ export default function LoginPage() {
               <input
                 className="px-2 py-1 border  rounded-md border-slate-500 outline-none"
                 type="password"
-                name=""
-                id=""
+                onChange={(e) => setData({ ...data, password: e.target.value })}
               ></input>
             </div>
             <div className="flex flex-col gap-2">
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routing/route";
+import { Toaster } from "@/components/ui/toaster";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
+    <Toaster />
   </React.StrictMode>
 );
